d6787972626d00e478aa8ab030cd09e2ddcac670
Mitch de Vries <Mitch@iMac-van-Mitch.local>
2019-03-24 12:52:19 +0100

    Added max and min temperature to output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability * 100}% chance of rain`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability * 100}% chance of rain. The temperature for today has a low of ${body.daily.data[0].temperatureMin} degrees and a high of ${body.daily.data[0].temperatureMax} degrees.`)
         }
     })
 }
